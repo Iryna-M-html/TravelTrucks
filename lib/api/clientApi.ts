925db8b9309a4ecab87be7a8ebfd03925e9a0d95
Iryna-M-html <ivmohnataya@gmail.com>
2026-01-06 01:34:59 +0200

    cleared structure

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,5 +1,5 @@
 import type { Camper } from "../../types/camper";
-// import { User, RegisterRequest, LoginRequest, EditRequest } from "@/types/user";
+
 import { nextServerApi } from "./api";
 import { CamperQueryParams, FetchCampersParams } from "@/types/filters";
 export interface FetchCampersResponse {
@@ -8,17 +8,6 @@ export interface FetchCampersResponse {
   totalPages: number;
 }
 
-// interface FetchNotesParams {
-//   page?: number;
-//   search?: string;
-//   perPage?: number;
-// }
-
-// export interface NewNotePayload {
-//   title: string;
-//   content: string;
-//   tag: NoteTag;
-// }
 export const fetchCampers = async ({
   page = 1,
   limit = 4,
@@ -47,61 +36,4 @@ export const fetchCampers = async ({
 //   }
 //   const response = await nextServerApi.get<Note>(`/notes/${id}`);
 //   return response.data;
-// };
-
-// export const getSingleNote = fetchNoteById;
-
-// export const createNote = async (noteData: NewNotePayload): Promise<Note> => {
-//   const response = await nextServerApi.post<Note>("/notes", noteData);
-//   return response.data;
-// };
-
-// export const deleteNote = async (id: string): Promise<Note> => {
-//   if (!id) {
-//     throw new Error("Note ID is required for deletion");
-//   }
-//   const response = await nextServerApi.delete<Note>(`/notes/${id}`);
-//   return response.data;
-// };
-
-// export interface RegisterRequest {
-//   email: string;
-//   password: string;
-//   //username: string;
-// }
-// export const register = async (body: RegisterRequest) => {
-//   const { data } = await nextServerApi.post<User>(`/auth/register`, body);
-//   return data;
-// };
-
-// export interface LoginRequest {
-//   email: string;
-//   password: string;
-// }
-// export const login = async (body: LoginRequest) => {
-//   const { data } = await nextServerApi.post<User>(`/auth/login`, body);
-//   return data;
-// };
-
-// export interface EditRequest {
-//   // email: string;
-//   username: string;
-// }
-// export const editUser = async (body: EditRequest) => {
-//   const { data } = await nextServerApi.patch<User>(`/users/me`, body);
-//   return data;
-// };
-
-// export const checkSession = async (): Promise<SessionResponse> => {
-//   const { data } = await nextServerApi.get<SessionResponse>(`/auth/session`);
-//   return data;
-// };
-
-// export const getMe = async () => {
-//   const { data } = await nextServerApi.get<User>(`/users/me`);
-//   return data;
-// };
-
-// export const logOut = async () => {
-//   await nextServerApi.post(`/auth/logout`);
 // };
