be65c0d729cc75af773d82c236252f95b8065751
Iryna-M-html <ivmohnataya@gmail.com>
2026-01-06 00:52:24 +0200

    style headers

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,25 +1,32 @@
 "use client";
 
 import Link from "next/link";
+import css from "./Header.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
   return (
-    <header>
-      <div className="container">
+    <div className="container">
+      <header className={css.header}>
         <div>
           {/* Logo */}
-          <Link href="/">
+          <Link href="/" className={css.logoLink}>
             {/* <svg width={136} height={16}>
               <use href="/  "></use>
             </svg> */}
           </Link>
 
           {/* Desktop navigation */}
-          <ul>
-            <li>
+          <ul className={css.navigation}>
+            <li className={pathname === "/" ? css.active : css.navigationItem}>
               <Link href="/">Home</Link>
             </li>
-            <li>
+            <li
+              className={
+                pathname === "/catalog" ? css.active : css.navigationItem
+              }
+            >
               <Link href="/catalog">Catalog</Link>
             </li>
           </ul>
@@ -34,7 +41,7 @@ export default function Header() {
             <Menu />
           </div> */}
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 }
