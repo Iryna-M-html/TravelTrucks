818157cf11a78956933174d55d4c6e3c3fa4d413
Iryna-M-html <ivmohnataya@gmail.com>
2026-01-07 11:44:00 +0200

    create content for campreid page

--- app/catalog/[id]/page.tsx
+++ app/catalog/[id]/page.tsx
@@ -1,4 +1,6 @@
 import { fetchCamperById } from "@/lib/api/clientApi";
+import css from "./camperid.module.css";
+import Image from "next/image";
 
 export default async function CamperDetailsPage({
   params,
@@ -8,8 +10,28 @@ export default async function CamperDetailsPage({
   const camper = await fetchCamperById(params.id);
 
   return (
-    <main>
+    <div>
       <h1>{camper.name}</h1>
-    </main>
+      <div className={css.meta}>
+        <span className={css.rating}>⭐{camper.rating} </span>
+        <span className={css.location}>{camper.location}</span>
+      </div>
+      <span className={css.price}>€{camper.price}.00</span>
+      <ul className={css.galleryList}>
+        {camper.gallery.map((item, index) => (
+          <li key={index} className={css.galleryItem}>
+            <Image
+              src={item.original || item.thumb}
+              alt={`${camper.name} ${index}`}
+              width={292}
+              height={310}
+              className={css.galleryImage}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <p className={css.description}>{camper.description}</p>
+    </div>
   );
 }
