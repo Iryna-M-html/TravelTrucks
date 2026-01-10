65acf92882501e99a78d428d1dfc32033168b6dc
Iryna-M-html <ivmohnataya@gmail.com>
2026-01-10 18:27:08 +0200

    fixed fill icon gas and mocrowave

--- app/catalog/[id]/page.tsx
+++ app/catalog/[id]/page.tsx
@@ -13,30 +13,32 @@ export default async function CamperDetailsPage({
 
   return (
     <div className="container">
-      <h1 className="name">{camper.name}</h1>
-      <div className={css.meta}>
-        <span className={css.rating}>⭐{camper.rating} </span>
-        <span className={css.location}>{camper.location}</span>
-      </div>
-      <span className={css.price}>€{camper.price}.00</span>
-      <ul className={css.galleryList}>
-        {camper.gallery.map((item, index) => (
-          <li key={index} className={css.galleryItem}>
-            <Image
-              src={item.original || item.thumb}
-              alt={`${camper.name} ${index}`}
-              width={292}
-              height={310}
-              className={css.galleryImage}
-            />
-          </li>
-        ))}
-      </ul>
-      <p className={css.description}>{camper.description}</p>
+      <section className={css.pageWrapper}>
+        <h1 className="name">{camper.name}</h1>
+        <div className={css.meta}>
+          <span className={css.rating}>⭐{camper.rating} </span>
+          <span className={css.location}>{camper.location}</span>
+        </div>
+        <span className={css.price}>€{camper.price}.00</span>
+        <ul className={css.galleryList}>
+          {camper.gallery.map((item, index) => (
+            <li key={index} className={css.galleryItem}>
+              <Image
+                src={item.original || item.thumb}
+                alt={`${camper.name} ${index}`}
+                width={292}
+                height={310}
+                className={css.galleryImage}
+              />
+            </li>
+          ))}
+        </ul>
+        <p className={css.description}>{camper.description}</p>
 
-      <div className={css.content}>
-        <NavigationTabs camper={camper} />
-      </div>
+        <div className={css.content}>
+          <NavigationTabs camper={camper} />
+        </div>
+      </section>
     </div>
   );
 }
